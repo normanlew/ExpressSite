9b9092a1c61ffec7be0df3b9b72fd414c1da864a
normanlew <normanlew@users.noreply.github.com>
2020-05-05 13:25:01 -0700

    Commit

--- app.js
+++ app.js
@@ -5,19 +5,43 @@ const path = require('path');
 const app = express();
 
 // view engine setup
-app.set(express.static(path.join(__dirname, 'views')));
 app.set('view engine', 'pug');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('public'));
 
-app.use('/', (req, res, next) => {
+app.get('/about', (req, res, next) => {
+  console.log('test2');
+  res.render('about');
+});
+
+app.get('/projects/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].id === id) {
+      return res.render('project', {theProject: projects[i]});
+    }
+  }
+  next();
+
+});
+
+app.get('/', (req, res, next) => {
   console.log('test');
   res.render('index', { projects });
 });
 
-app.use('/about', (req, res, next) => {
-  console.log('test2');
-  res.render('about');
+// This error handler was taken from a sample lecture at teamtreehouse.com
+app.use( ( req, res, next) =>  {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use( (err, req, res, next) => {
+  res.locals.error = err;
+  // res.status(err.status);
+  res.render('error');
 });
 
 app.listen(3000, () => {
